51fdd2af0dcd2d85bb7e099421c9727b8e39638f
Roman Hladun <rec2002@gmail.com>
2023-12-02 17:55:02 +0200

    Summary section

---
+++ src/http/summary-api.js
@@ -0,0 +1,8 @@
+import api from './api'
+
+const apiPath = import.meta.env.VITE_API_PATH;
+
+const resource = apiPath + "/summaries";
+
+export const tasksSummary = () => api.get(resource)
+
---
+++ src/stores/summary.js
@@ -0,0 +1,22 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { tasksSummary } from '../http/summary-api'
+
+
+export const useSummaryStore = defineStore('summaryStore', ()=>{
+
+    const summaries = ref([]);
+
+    const fetchTasksSummary = async ()=> {
+
+        const {data} = await tasksSummary();
+
+        summaries.value = data
+    }
+
+    return {
+        summaries, 
+        fetchTasksSummary
+    }
+
+});
